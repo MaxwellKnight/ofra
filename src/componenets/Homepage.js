05a3116a8b3b8fd722b575e57187faace619cdc4
Maxwell Knight <maxwell.knight98@gmail.com>
2023-02-20 15:26:59 +0200

    new font changes

--- src/componenets/Homepage.js
+++ src/componenets/Homepage.js
@@ -1,3 +1,4 @@
+import '../fonts/miri/miriwin-webfont.ttf';
 import '../css/homePage.css'
 import { printClouds } from '../utils/print'
 const { clouds } = require('../data/clouds.js')
@@ -5,36 +6,38 @@ const backgroundImage = require('../images/main-background.jpeg')
 
 const Homepage = () => {
 
-    return (
-        <main className="main-content" dir="rtl">
-            <section className="main-hero">
-                <img src={backgroundImage} alt="" className="main-image" />
-                <h2 className="main-title">איזון גוף-נפש-רוח </h2>
-                <h2 className="main-title">בדרך חוויתית </h2>
-                <p className="main-subtitle">בשילוב - שיח, הקשבה, יוגה, נשימות, מדיטציה, מיינדפולנס, צלילים ועוד...</p>
-                <p className="main-text">המיפגשים שלי עם אנשים בקבוצה וגם עם האדם, שאותו אני מלווה באופן פרטני,
-                    ממחישים לי כל פעם מחדש את הכוח של נתינה וקבלה בתוך מרחב
-                    של תנועה, שיח והקשבה - בו מגלה כל אחד את זכות הבחירה, כדי
-                    לשנות דפוסי-התנהגות כלפי המציאות, ואיך ניתן לשחרר את מה שחוסם
-                    ולהפוך אותו למשהו חיובי ומשמעותי יותר.
-                    לפעמים צריך רק מישהו אחד שיקשיב, שיכיל, שיתמוך וישקף
-                    את מה שבאמת קורה, לגלות את האור שבתוכינו ולסמוך.
-                    מוצאת את עצמי בהודיה כל פעם מחדש, על כך שזכיתי ביכולת לאפשר
-                    לאחר, לראות את האור שנמצא בתוכו ולדעת שיש לי חלק קטן בגילוי הזה,
-                    שמוביל לשינוי הרצוי.</p>
-            </section>
+	return (
+		<main className="main-content" dir="rtl">
+			<section className="main-hero">
+				<img src={backgroundImage} alt="" className="main-image" />
+				<h1 className="yoga-way-title">איזון גוף-נפש-רוח </h1>
+				<h2 className="yoga-way-title-main">בדרך היוגה </h2>
+				<p className="yoga-way-sub">הדרכה וליווי בתהליכי התפתחות</p>
+				<p className="yoga-way-sub">בהיבט רגשי-חברתי</p>
+				<h2 className="yoga-way-title-main">פרטני וקבוצתי</h2>
+				<p className="main-text">המיפגשים שלי עם אנשים בקבוצה וגם עם האדם, שאותו אני מלווה באופן פרטני,
+					ממחישים לי כל פעם מחדש את הכוח של נתינה וקבלה בתוך מרחב
+					של תנועה, שיח והקשבה - בו מגלה כל אחד את זכות הבחירה, כדי
+					לשנות דפוסי-התנהגות כלפי המציאות, ואיך ניתן לשחרר את מה שחוסם
+					ולהפוך אותו למשהו חיובי ומשמעותי יותר.
+					לפעמים צריך רק מישהו אחד שיקשיב, שיכיל, שיתמוך וישקף
+					את מה שבאמת קורה, לגלות את האור שבתוכינו ולסמוך.
+					מוצאת את עצמי בהודיה כל פעם מחדש, על כך שזכיתי ביכולת לאפשר
+					לאחר, לראות את האור שנמצא בתוכו ולדעת שיש לי חלק קטן בגילוי הזה,
+					שמוביל לשינוי הרצוי.</p>
+			</section>
 
-            <section id='cloud-section' className="cloud-sentences">
-                <h1>משפטים שמלווים אותי לאורך הדרך</h1>
-                <p><strong className='cloud-p'>*כדי לקרוא לחצ/י על הענן</strong></p>
-                <div className='clouds-only-container'>
-                    <div className="clouds"> {/* Printing all the clouds with quotes */}
-                        {printClouds(clouds)}
-                    </div>
-                </div>
-            </section>
-        </main>
-    )
+			<section id='cloud-section' className="cloud-sentences">
+				<h1>משפטים שמלווים אותי לאורך הדרך</h1>
+				<p><strong className='cloud-p'>*כדי לקרוא לחצ/י על הענן</strong></p>
+				<div className='clouds-only-container'>
+					<div className="clouds"> {/* Printing all the clouds with quotes */}
+						{printClouds(clouds)}
+					</div>
+				</div>
+			</section>
+		</main>
+	)
 }
 
 export default Homepage;
