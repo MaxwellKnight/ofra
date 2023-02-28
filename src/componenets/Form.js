a0305372c8f3c2d2bdb393b3785f923ca6694c6d
Maxwell Knight <maxwell.knight98@gmail.com>
2023-02-28 07:25:10 +0200

    fixed contact form

--- src/componenets/Form.js
+++ src/componenets/Form.js
@@ -3,72 +3,72 @@ import '../css/form.css'
 
 const Form = () => {
 
-    const [formData, setFormData] = useState({
-        fullName: '',
-        email: '',
-        phone: '',
-        notes: 'לדוגמא: פנייה בנושא ליווי מורות'
-    })
+	const [formData, setFormData] = useState({
+		fullName: '',
+		email: '',
+		phone: '',
+		notes: 'לדוגמא: פנייה בנושא ליווי מורות'
+	})
 
-    const handleFormData = event => {
-        const name = event.target.name
-        const value = event.target.value
-        setFormData(prevFormData => ({
-            ...prevFormData,
-            [name]: value
-        }))
-    }
+	const handleFormData = event => {
+		const name = event.target.name
+		const value = event.target.value
+		setFormData(prevFormData => ({
+			...prevFormData,
+			[name]: value
+		}))
+	}
 
-    return (
-        <div className='contact-form'>
-            <form action='#'>
+	return (
+		<div className='contact-form'>
+			<form action='#'>
 
-                <label htmlFor='name'>שם</label>
-                <input
-                    id='name'
-                    name='fullName'
-                    value={formData.fullName}
-                    type='text'
-                    placeholder='דור ישראלי'
-                    onChange={handleFormData}
-                    required
-                />
+				<label htmlFor='name'>שם</label>
+				<input
+					id='name'
+					name='fullName'
+					value={formData.fullName}
+					type='text'
+					placeholder=''
+					onChange={handleFormData}
+					required
+				/>
 
-                <label htmlFor='phone'>מס' נייד</label>
-                <input
-                    id='phone'
-                    name='phone'
-                    value={formData.phone}
-                    type='phone'
-                    placeholder='053-123-4567'
-                    onChange={handleFormData}
-                    required
-                />
+				<label htmlFor='phone'>מס' נייד</label>
+				<input
+					id='phone'
+					name='phone'
+					value={formData.phone}
+					type='phone'
+					placeholder=''
+					onChange={handleFormData}
+					required
+				/>
 
-                <label htmlFor='email'>דואר אלקטרוני</label>
-                <input
-                    id='email'
-                    name='email'
-                    value={formData.email}
-                    type='email'
-                    placeholder='dor@gmail.com'
-                    onChange={handleFormData}
-                    required
-                />
+				<label htmlFor='email'>דואר אלקטרוני</label>
+				<input
+					id='email'
+					name='email'
+					value={formData.email}
+					type='email'
+					placeholder=''
+					onChange={handleFormData}
+					required
+				/>
 
-                <label htmlFor='notes'>הערות</label>
-                <textarea
-                    id='notes'
-                    name='notes'
-                    value={formData.notes}
-                    type='textarea'
-                    onChange={handleFormData}
-                    required
-                ></textarea>
-                <button type='submit'>שלח/י</button>
-            </form>
-        </div>
-    )
+				<label htmlFor='notes'>הערות</label>
+				<textarea
+					id='notes'
+					name='notes'
+					value={formData.notes}
+					type='textarea'
+					onChange={handleFormData}
+					required
+				></textarea>
+				<button type='submit'>שלח/י</button>
+			</form>
+		</div>
+	)
 }
 
 export default Form
