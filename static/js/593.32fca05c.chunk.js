735786baed62d4af102d51596becf3d33c88ede8
Maxwell Knight <maxwell.knight98@gmail.com>
2024-01-15 01:07:20 +0200

    Updates

--- static/js/593.32fca05c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_training=self.webpackChunkreact_training||[]).push([[593],{4593:function(t,n,r){r.r(n),r.d(n,{default:function(){return i}});r(2791);var e=r(3451),a=r(184),i=function(t){var n=t.data,r=t.toggleIsFormShown;return(0,a.jsx)("div",{className:"item",dir:"rtl",children:(0,e.tq)([0,1,4,7,8],n,r)},n.id)}}}]);
-//# sourceMappingURL=593.32fca05c.chunk.js.map
